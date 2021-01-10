83a47df9fd9095120a15fb7e2c9f4a9828eb7dc0
Kisik Misik <700mmr@gmail.com>
2021-01-10 23:10:06 +0100

    base file structure + navigation

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+let Contact = (props) => {
+    return (
+        <section className="home">
+            <h2 className="hidden">Contact us</h2>
+            
+        </section>
+    )
+}
+
+export default Contact;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Products = (props) => {
+    return (
+        <section className="products">
+            <h2 className="hidden">Products</h2>
+        </section>
+    )
+}
+
+export default Products;
