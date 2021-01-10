b53b0ae6425664d3253ae8a5ca3cfde4142ec944
Kisik Misik <700mmr@gmail.com>
2021-01-10 20:49:49 +0100

    project init

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Footer = (props) => {
+    return (
+        <section className="footer">
+            <h2 className="hidden">Footer</h2>
+        </section>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let Header = (props) => {
+    return (
+        <section className="header">
+            <h2 className="hidden">Header</h2>
+        </section>
+    )
+}
+
+export default Header;
